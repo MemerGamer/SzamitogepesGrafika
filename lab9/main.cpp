7bd3c14d39b585d143c4ddcaa1ca743872d6142e
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-11-15 13:56:54 +0200

    patched earth

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,16 +1,18 @@
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <SOIL/SOIL.h>
+#include <string>
+#include <iostream>
 
 GLuint cubeTextures[6]; // Array to store texture IDs
 GLuint sphereTexture;
 GLfloat cubeVertices[][12] = {
-        {-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 1.0f},  // Front face
-        {-1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f, -1.0f,  -1.0f, 1.0f, -1.0f},  // Back face
-        {1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f, -1.0f,  1.0f, 1.0f, 1.0f},  // Right face
-        {-1.0f, -1.0f, -1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,  -1.0f, 1.0f, -1.0f},  // Left face
-        {-1.0f, 1.0f, 1.0f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f, -1.0f,  -1.0f, 1.0f, -1.0f},  // Top face
-        {-1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f}  // Bottom face
+        {-1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,  1.0f},  // Front face
+        {-1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f},  // Back face
+        {1.0f,  -1.0f, 1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  1.0f},  // Right face
+        {-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f},  // Left face
+        {-1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f},  // Top face
+        {-1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f}  // Bottom face
 };
 
 float rotationX = 0.0f;
@@ -28,6 +30,11 @@ void loadTexture(const char *filename, GLuint *textureID) {
             SOIL_CREATE_NEW_ID,
             SOIL_FLAG_INVERT_Y
     );
+
+    if (*textureID == 0) {
+        // Handle the error, e.g., print an error message or exit the program.
+        std::cerr << "Error loading texture: " << filename << std::endl;
+    }
 }
 
 void keyboard(unsigned char key, int x, int y) {
@@ -68,10 +75,14 @@ void drawTexturedCube() {
     for (int i = 0; i < 6; ++i) {
         glBindTexture(GL_TEXTURE_2D, cubeTextures[faceTextures[i]]);
         glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(cubeVertices[i][0], cubeVertices[i][1], cubeVertices[i][2]);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(cubeVertices[i][3], cubeVertices[i][4], cubeVertices[i][5]);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(cubeVertices[i][6], cubeVertices[i][7], cubeVertices[i][8]);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(cubeVertices[i][9], cubeVertices[i][10], cubeVertices[i][11]);
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3f(cubeVertices[i][0], cubeVertices[i][1], cubeVertices[i][2]);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3f(cubeVertices[i][3], cubeVertices[i][4], cubeVertices[i][5]);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3f(cubeVertices[i][6], cubeVertices[i][7], cubeVertices[i][8]);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3f(cubeVertices[i][9], cubeVertices[i][10], cubeVertices[i][11]);
         glEnd();
     }
 }
@@ -83,6 +94,38 @@ void drawTexturedSphere() {
     gluQuadricNormals(quadric, GLU_SMOOTH);
     gluSphere(quadric, 1.0, 50, 50);
     gluDeleteQuadric(quadric);
+}
+
+
+void drawText(const std::string& text, float x, float y) {
+    // Save the current state of texturing
+    GLboolean isTextureEnabled = glIsEnabled(GL_TEXTURE_2D);
+
+    // Disable texturing for rendering text
+    glDisable(GL_TEXTURE_2D);
+
+    glMatrixMode(GL_PROJECTION);
+    double matrix[16];
+    glGetDoublev(GL_PROJECTION_MATRIX, matrix);
+    glLoadIdentity();
+    glOrtho(0.0f, 800.0f, 0.0f, 600.0f, -5.0f, 5.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();
+    glLoadIdentity();
+    glRasterPos2f(x, y);
+    for (char i : text) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, i);
+    }
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glLoadMatrixd(matrix);
+    glMatrixMode(GL_MODELVIEW);
+
+    // Restore the previous state of texturing
+    if (isTextureEnabled) {
+        glEnable(GL_TEXTURE_2D);
+    }
 }
 
 void display() {
@@ -98,12 +141,14 @@ void display() {
         // Draw the textured cube
         drawTexturedCube();
     } else {
-        
+
 
         // Draw the textured sphere
         drawTexturedSphere();
     }
-
+    // Display message in the left corner
+    std::string message = "Press 'c' to switch between cube and sphere";
+    drawText(message, 10.0f, 10.0f);
     glutSwapBuffers();
 }
 
@@ -128,11 +173,10 @@ void mouse(int button, int state, int x, int y) {
 }
 
 
-
 void motion(int x, int y) {
     if (isMousePressed) {
-        rotationX += (y - lastMouseY) * 0.1f;
-        rotationY += (x - lastMouseX) * 0.1f;
+        rotationX += (float(y) - float(lastMouseY)) * 0.1f;
+        rotationY += (float(x) - float(lastMouseX)) * 0.1f;
         lastMouseX = x;
         lastMouseY = y;
         glutPostRedisplay();
@@ -142,7 +186,7 @@ void motion(int x, int y) {
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutCreateWindow("Textured Cube");
+    glutCreateWindow("Textured Cube and Sphere");
 
     init();
 
